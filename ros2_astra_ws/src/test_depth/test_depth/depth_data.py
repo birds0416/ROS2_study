c12f5015aab4299cc2f68af9fcaf532280ef9e65
RoyLim <68123073+birds0416@users.noreply.github.com>
2023-12-15 13:27:48 +0900

    update NAMESPACE settings

--- ros2_astra_ws/src/test_depth/test_depth/depth_data.py
+++ ros2_astra_ws/src/test_depth/test_depth/depth_data.py
@@ -6,8 +6,15 @@ class DepthData(Node):
     def __init__(self):
         super().__init__('DepthData')
 
-        self.sub_depth_dat = self.create_subscription(PointCloud2, '/camera/depth/points', self.depth_callback, 10)
+        self.NAMESPACE = self.get_namespace()
+        self.get_logger().info("Namespace: {}".format(self.NAMESPACE))
+
+        self.sub_depth_dat = self.create_subscription(PointCloud2, self.NAMESPACE + '/camera/depth/points', self.depth_callback, 10)
         self.depth_points = PointCloud2()
+
+        self.pub_depth = self.create_publisher(PointCloud2, self.NAMESPACE + '/depth_points', 10)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self._callback)
 
     def depth_callback(self, data):
         if data != None:
@@ -33,6 +40,12 @@ class DepthData(Node):
 
             # for d in points:
             #     self.get_logger().info("Depth Points are {}".format(d))
+    
+    def _callback(self):
+        msg = PointCloud2()
+        msg = self.depth_points
+        self.pub_depth.publish(msg)
+        self.get_logger().info("Publishing: {}".format(msg))
 
 def main(args=None):
     rclpy.init(args=args)
